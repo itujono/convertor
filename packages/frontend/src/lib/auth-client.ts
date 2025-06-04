7a143702c4a7eea629c04b50eb7e43d9e1c54aa9
itujono <itujono@gmail.com>
2025-06-04 13:14:47 +0700

    Update db import on frontend.

--- packages/frontend/src/lib/auth-client.ts
+++ packages/frontend/src/lib/auth-client.ts
@@ -1,10 +1,9 @@
 import { createClient } from "@supabase/supabase-js";
-import { Database } from "db/schema";
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
 
-export const supabase = createClient<Database>(supabaseUrl, supabaseAnonKey);
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
 export type AuthUser = {
   id: string;
